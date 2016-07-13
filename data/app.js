8920f5bfa51d23c1d18f8214cafd5525b865d46d
Kaeside Iwuagwu <kaeside.iwuagwu@gmail.com>
2016-07-13 12:08:13 -0400

    deleted routes folder and added boilerplate socket.io connection in app.js

---
+++ data/app.js
@@ -0,0 +1,15 @@
+var socket_io = require('socket.io');
+var http = require('http');
+var express = require('express');
+
+var app = express();
+app.use(express.static('public'));
+
+var server = http.Server(app);
+var io = socket_io(server);
+
+io.on('connection', function (socket) {
+    console.log('Client connected');
+});
+
+server.listen(8080);
